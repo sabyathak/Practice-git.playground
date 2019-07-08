58fb08bb687ba0ac5af5369850cc0befcef5211c
Kaiba Murphy <kautmnm121@gmail.com>
2019-07-08 11:54:29 -0700

    First commit

---
+++ Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+//comments!
+
+//code that would take each scholar's name and output each mane into a string
+//var name  = "Sabyatha"
+//var name2 = "Nailah"
+//print("Hi \(name) and \(name2)! We are so happy you're here at KWK Portland!")
+
+
+//working with integers and doubles
+//Data Types, Ints and Doubles
+
+//var int = 4
+//var double = 5.0
+//print(Double(int) * double)
+
+var a = 12.4
+var b = 65.0
+var c = 31.0
+var d = 98
+print(((a + b + c ) + Double(d))/4 )
+//var average = (a + b + c + d)/4
+//print(average)
+
+
+
+
